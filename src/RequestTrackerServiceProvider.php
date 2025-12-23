6fbf4c2a208c9603289ddf873a7ff3d1b316abf0
BurningYolo <hishamzahid123@gmail.com>
2025-12-23 16:14:56 +0500

    revert previous commit

--- src/RequestTrackerServiceProvider.php
+++ src/RequestTrackerServiceProvider.php
@@ -60,8 +60,8 @@ class RequestTrackerServiceProvider extends ServiceProvider
     public function register()
     {
         $this->mergeConfigFrom(
-            __DIR__.'/../config/http-monitor.php',
-            'http-monitor-config'
+            __DIR__.'/../config/request-tracker.php',
+            'request-tracker'
         );
 
         // Delay until Laravel is fully booted (Laravel 9–11 safe)
